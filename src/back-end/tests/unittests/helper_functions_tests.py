87594990ac9ec99d93c3a9798c1c0cabc2adc8f3
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2023-03-29 16:07:03 +0200

    Rename helper functions test file and correct its docstring

--- src/back-end/tests/unittests/helper_functions_tests.py
+++ src/back-end/tests/unittests/helper_functions_tests.py
@@ -10,7 +10,10 @@ class TestHelper(unittest.TestCase):
         pass
 
     def test_get_path_from_root_test(self):
-        """Tries to load goodtileexample.png from unittests folder."""
+        """ 
+        Tries to convert a path written relative to project root, to path relative 
+        to system root and checks if resulting path is valid. 
+        """
         path = hf.get_path_from_root("/utility/helper_functions.py")
         self.assertTrue(os.path.exists(path))
 
