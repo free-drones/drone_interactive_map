e9d95e924fabb4cf8c128c265b2e9d334ac07739
Noah Larsson <Noah_Larsson@hotmail.com>
2023-04-05 16:16:30 +0200

    Add and improve docstrings

--- src/back-end/IMM/drone_allocator/area_segmentation.py
+++ src/back-end/IMM/drone_allocator/area_segmentation.py
@@ -2,28 +2,8 @@ import numpy as np
 from bisect import insort
 import mapbox_earcut as earcut
 
-# Polygon
-# node_list
-#
-# Bygga nodnät
-# segments(source_position, num_active_drones) - kan ha utanför Polygon-klassen
-# - Bestämma antal linjer
-# - Stråla ut linjer med vinkel
-# - (Iterativt) hitta vinkel på varje stråle för jämn indelning (av antal noder)  
-# - 
-# - Skapa nya polygoner från indelning
-# - 
-#
-# Identifiera vilka noder som finns i Polygon
-# 
-# Triangulation 
-#
 
-# Utility:
-#
-#  
-#
-#
+
 class Triangle():
     def __init__(self, nodes):
         self.a = nodes[0]
@@ -33,7 +13,7 @@ class Triangle():
         self.bounding_box = self.create_bounding_box()
 
     def isConvex(self):
-        # Checks if the angle formed by BAC
+        """ Return True if the angle formed by BAC is convex """
         ab = (self.b.x - self.a.x, self.b.y - self.a.y)
         ac = (self.c.x - self.a.x, self.c.y - self.a.y)
         
@@ -42,13 +22,13 @@ class Triangle():
 
     @staticmethod
     def area(a, b, c):
-        # Return the area of a triangle formed by the nodes a, b and c
+        """ Return the area of a triangle formed by the nodes a, b and c """
         return abs((a.x * (b.y - c.y) \
 		+ b.x * (c.y - a.y) \
 		+ c.x * (a.y - b.y)) / 2.0)
     
     def create_bounding_box(self):
-        # Calculate the bounding box of the triangle and store extreme points in a dictionary
+        """ Calculate the bounding box of the triangle, store and return extreme points in a dictionary """
         dic = {}
         x_coords = [node.x for node in self.nodes()]
         y_coords = [node.y for node in self.nodes()]
@@ -59,13 +39,13 @@ class Triangle():
         return dic
 
     def check_bounding_box(self, point):
-        # Check if the given point is within the bounding box
+        """ Return True if the given point is within the bounding box """
         outside_x = point.x < self.bounding_box["x_min"] or point.x > self.bounding_box["x_max"]
         outside_y = point.y < self.bounding_box["y_min"] or point.y > self.bounding_box["y_max"]
         return not (outside_x or outside_y)
 
     def contains(self, point):
-        # Check if the given point is within the triangle
+        """ Return True if the given point is within the triangle """
         if not self.check_bounding_box(point):
             return False
         ABC_area = Triangle.area(self.a, self.b, self.c)
@@ -76,7 +56,7 @@ class Triangle():
         return ABC_area == PBC_area + PAC_area + PAB_area
 
     def nodes(self):
-        # Return the nodes of the triangle as a list
+        """ Return the nodes of the triangle as a list """
         return [self.a, self.b, self.c]
 
 
@@ -88,18 +68,22 @@ class Polygon():
         self.node_grid = []
         self.segments = []
     
-    def gogo_gadget(self, node_spacing, start_location, num_seg):
-        # Find area segments
+    def create_area_segments(self, node_spacing, start_location, num_seg):
+        """ This is the main function to perform area segmentation and calls the necessary helper functions in correct order
+            Create area segments of the polygon instance:
+                1. Triangulate the polygon
+                2. Define the polygons bounding box
+                3. Create a node grid inside the polygon
+                4. Segmentate the polygon into 'num_seg' segments
+        """
         self.triangles = self.earcut_triangulate()
         self.bounding_box = self.create_bounding_box()
         self.node_grid = self.create_node_grid(node_spacing, start_location)
         self.segments = self.create_segments(num_seg)
-        #........... Fortsättning följer ;)
-        #
-        #
+
 
     def earcut_triangulate(self):
-        # Triangulate the polygon and return the triangles as a list
+        """ Triangulate the polygon using the Ear Clipping algorithm and return the triangles as a list """
         nodes = np.array([node() for node in self.nodes]).reshape(-1, 2)
         rings = np.array([len(nodes)])
         result = earcut.triangulate_int32(nodes, rings)
@@ -110,18 +94,8 @@ class Polygon():
 
         return triangles
     
-    def create_segments(self, num_seg):
-        total_node_count = 0
-        segments = []
-        num_nodes = int(np.ceil(len(self.node_grid) / num_seg))
-        while total_node_count < len(self.node_grid):
-            seg_nodes = self.node_grid[total_node_count : total_node_count + num_nodes]
-            segments.append(Segment(seg_nodes))
-            total_node_count += num_nodes
-        return segments
-
     def create_bounding_box(self):
-        # Calculate the bounding box of the polygon and store extreme points in a dictionary
+        """ Calculate the bounding box of the polygon, store and return its extreme points in a dictionary """
         dic = {}
         x_coords = [node.x for node in self.nodes]
         y_coords = [node.y for node in self.nodes]
@@ -132,7 +106,9 @@ class Polygon():
         return dic 
 
     def create_node_grid(self, node_spacing, start_location):
-        # Create a node grid for the polygon
+        """ Evenly distribute a node grid inside the polygon and return a sorted list of the nodes according to the nodes angle relative 'start_location' 
+            'node_spacing' decides the distance between each node in the created grid
+        """
         node_grid = []
         self.bounding_box = self.create_bounding_box()
         max_diff_ang = self.max_diff_angle(start_location)
@@ -143,15 +119,28 @@ class Polygon():
                     if triangle.contains(node):
                         node.angle_to_start = start_location.angle_to(node)
                         node_grid.append(node)
-                        #insort(node_grid, node, key=lambda n: n.angle_to_start)
                         break
         node_grid.sort(key=lambda n: (n.angle_to_start - max_diff_ang), reverse=False)
-        return node_grid
+        return node_grid    
+    
+    def create_segments(self, num_seg):
+        """ Evenly partition the amount of node grid nodes into segments 
+            Node grid list is presumed to be sorted according to angle relative 'start_location' so resulting segments will be lines drawn from 'start_location'      
+        """
+        total_node_count = 0
+        segments = []
+        num_nodes = int(np.ceil(len(self.node_grid) / num_seg))
+        while total_node_count < len(self.node_grid):
+            seg_nodes = self.node_grid[total_node_count : total_node_count + num_nodes]
+            segments.append(Segment(seg_nodes))
+            total_node_count += num_nodes
+        return segments
+
+
 
     def max_diff_angle(self, start_location):
-        # Calculate the difference between (start_location, a) and (start_location, b) for
-        # each node combination (a and b), and return the angle from the pair with the greatest
-        # difference
+        """ Calculate the difference between (start_location, a) and (start_location, b) for
+         each node combination (a and b), and return the angle from the pair with the greatest difference """
         max_separation = 0
         chosen_nodes = (None, None)
         for node_a in self.nodes:
@@ -166,7 +155,7 @@ class Polygon():
 
     @staticmethod
     def is_clockwise(nodes):
-        # Check if the polygon is clockwise
+        """ Return True if the polygon created by the 'nodes' list is defined in a clockwise order """
         signed_area = 0
         num_nodes = len(nodes)
 
@@ -187,9 +176,11 @@ class Node():
         self.angle_to_start = start_location if start_location else None
     
     def angle_to(self, other_node):
+        """ Return the angle of the node instance to the 'other_node' """
         return np.arctan2(other_node.y - self.y, other_node.x - self.x)
 
     def __call__(self):
+        """ Return the coordinates when a node object is called """
         return (self.x, self.y)
 
 class Segment():
