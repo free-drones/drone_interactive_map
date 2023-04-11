2ce7a1c35e415bb52ba54f2b280eba923366a42e
unknown <thomas@wiger.net>
2023-04-11 10:55:25 +0200

    deleting rise_drones

--- src/back-end/rise_drones/doc/source/todo.rst
+++
@@ -1,6 +0,0 @@
-.. _todo:
-
-Todo List
-=========
-
-.. todolist::
--- src/back-end/rise_drones/src/dss/__init__.py
+++
@@ -1,6 +0,0 @@
-'''Drone Safety Service'''
-
-__author__ = 'Lennart Ochel <>, Andreas Gising <andreas.gising@ri.se>, Kristoffer Bergman <kristoffer.bergman@ri.se>, Hanna Müller <hanna.muller@ri.se>, Joel Nordahl'
-__version__ = '1.3.0'
-__copyright__ = 'Copyright (c) 2019-2022, RISE'
-__status__ = 'development'
