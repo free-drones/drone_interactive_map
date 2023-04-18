d6720e82c280e627d2d0fa29e56129bec5aafc65
thopi836 <thopi836@student.liu.se>
2023-04-18 16:31:54 +0200

    Add suggestion for manual mode to drone_manager

--- src/back-end/IMM/drone_manager/drone_manager.py
+++ src/back-end/IMM/drone_manager/drone_manager.py
@@ -81,10 +81,21 @@ class DroneManager(Thread):
                 d.route = None
                 self.link.return_to_home(d)
         
+    def resource_management(self):
+        """ Handles assigning drones based on battery and mission status to routes. Drones are sent to charge when needed. """
+
+        for d in self.drones:
+            if self.link.get_drone_status(d) != "charging" and self.link.get_drone_battery(d) < MIN_CHARGE_LEVEL:
+                if d.route:
+                    d.route.drone = None
+                d.route = None
+                self.link.return_to_home(d)
+        
         for r in self.routes:
             # is there a drone that flies this route?
             if not r.drone:
-                for d in self.drones:
+                auto_drones = self.get_auto_drones() # adding for manual
+                for d in auto_drones:
                     battery_lvl = self.link.get_drone_battery(d)
                     drone_status = self.link.get_drone_status(d)
                     if not d.route and battery_lvl >= FULL_CHARGE_LEVEL and drone_status in ["landed", "waiting", "idle"]:
@@ -95,7 +106,81 @@ class DroneManager(Thread):
                 if not r.drone:
                     # area segmentation with # of drones that are "available" – what does that mean? etc
                     pass
+        
+#---------------------------------MANUAL START--------------------------------------------
+
+        if self.mode == "manual":
+            route = self.get_manual_route()
+            manual_drone = self.get_manual_drone()
+            if not manual_drone:
+                closest_drone = self.get_closest_auto_drone(self.get_manual_route())
+                if closest_drone:
+                    closest_drone.mode = "manual"
+                    closest_drone.route = self.get_manual_route()
+                    closest_drone.current_mission = self.create_mission(closest_drone.route)
+            else:
+                pos = self.link.get_drone_position(d)
+                distance_to_goal = self.distance(self.get_coordinates_from_route(route), (pos[0],pos[1]))
+                if distance_to_goal > ACCEPTABLE_DISTANCE:
+                    clostest_auto_drone, auto_drone_distance = self.get_closest_auto_drone(self.get_coordinates_from_route(route))
+                    if auto_drone_distance < distance_to_goal:
+                        manual_drone.mode = "auto"
+                        manual_drone.route = None
+                        manual_drone.current_mission = None
+                        clostest_auto_drone.mode = "manual"
+                        clostest_auto_drone.route = self.get_manual_route()
+                        clostest_auto_drone.current_mission = self.create_mission(clostest_auto_drone.route)
+
+    def get_coordinates_from_route(self, route):
+        coordinates = (lat, lon)
+        return coordinates
+
+    def get_manual_drone(self):
+        for d in self.drones:
+            if d.mode == "manual":
+                return d
+            else:
+                return None
             
+    def get_manual_route(self, drone):
+        return None
+    
+    def get_closest_auto_drone(self, coordinates):
+        """ Returns the closest drone to the given coordinates"""
+        drones = self.get_auto_drones()
+        if drones:
+            positions = {}
+            for d in drones:
+                pos = self.link.get_drone_position(d)
+                positions[d]= (pos['lat'], pos['lon'])
+            closest_drone, min_distance = min(((drone, self.distance(coordinates, positions[drone])) for drone in positions), key=lambda item: item[1])
+            return closest_drone, min_distance
+    
+    def distance(self, coordinates1, coordinates2):
+        """ Returns the distance between two coordinates"""
+        lat1, lon1 = coordinates1
+        lat2, lon2 = coordinates2
+
+        # Convert latitude and longitude from degrees to radians
+        rad_lat1, rad_lon1, rad_lat2, rad_lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+        # Calculate the distance on a sphere using the Haversine formula
+        lat_diff = rad_lat2 - rad_lat1
+        lon_diff = rad_lon2 - rad_lon1
+        x = math.sin(lat_diff/2)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) * math.sin(lon_diff/2)**2
+        sphere_dist = 2 * math.asin(math.sqrt(x))
+
+        # Radius of Earth in kilometers
+        R = 6371
+
+        # Calculate the distance on the earth's surface
+        distance = R * sphere_dist
+        return distance
+          
+    def get_auto_drones(self):
+        return [d for d in self.drones if d.mode == "auto"]
+    
+#---------------------------------MANUAL END --------------------------------------------
 
     def assign_missions(self):
         """ Creates missions and executes them for each drone that have a route to fly """
