382bf3e476ca309cb9a6c56e6f15cd52ae6837f9
A-N-D-R-O-V <snabelkaka15@hotmail.com>
2023-05-10 09:35:51 +0200

    Update area visualization

--- src/back-end/IMM/drone_allocator/area_segmentation.py
+++ src/back-end/IMM/drone_allocator/area_segmentation.py
@@ -3,7 +3,6 @@ from bisect import insort
 import mapbox_earcut as earcut
 
 
-
 class Triangle():
     def __init__(self, nodes):
         self.a = nodes[0]
@@ -48,12 +47,15 @@ class Triangle():
         """ Return True if the given point is within the triangle """
         if not self.check_bounding_box(point):
             return False
+        # Calculate the area of each sub-triangle created by the point and each pairwise combination of point a, b and c.
+        # The point is inside the triangle if the sum of these areas is the same as the area of the triangle itself.
         ABC_area = Triangle.area(self.a, self.b, self.c)
         PBC_area = Triangle.area(point, self.b, self.c)
         PAC_area = Triangle.area(point, self.a, self.c)
         PAB_area = Triangle.area(point, self.a, self.b)
         
-        return ABC_area == PBC_area + PAC_area + PAB_area
+        # ABC is the original triangle and PBC, PAC, PAB are the sub-triangles created with the parameter 'point'.
+        return int(ABC_area) == int(PBC_area + PAC_area + PAB_area)
 
     def nodes(self):
         """ Return the nodes of the triangle as a list """
@@ -61,14 +63,14 @@ class Triangle():
 
 
 class Polygon():
-    def __init__(self, nodes):
-        self.nodes = nodes
+    def __init__(self, node_list):
+        self.nodes = [Node((node_dict["lat"], node_dict["long"])) for node_dict in node_list]
         self.bounding_box = {}
         self.triangles = []
         self.node_grid = []
         self.segments = []
     
-    def create_area_segments(self, node_spacing, start_location, num_seg):
+    def create_area_segments(self, node_spacing, start_coordinates, num_seg):
         """ This is the main function to perform area segmentation and calls the necessary helper functions in correct order
             Create area segments of the polygon instance:
                 1. Triangulate the polygon
@@ -76,19 +78,37 @@ class Polygon():
                 3. Create a node grid inside the polygon
                 4. Segmentate the polygon into 'num_seg' segments
         """
-        self.triangles = self.earcut_triangulate()
-        self.bounding_box = self.create_bounding_box()
-        self.node_grid = self.create_node_grid(node_spacing, start_location)
-        self.segments = self.create_segments(num_seg)
+        start_location = Node(start_coordinates)
 
+        self.bounding_box = self.create_bounding_box()
+        self.triangles = self.earcut_triangulate()
+        self.node_grid = self.create_node_grid(node_spacing, start_location)
+        self.update_area_segments(start_location, num_seg)
+ 
+    def update_area_segments(self, start_location, num_seg):
+        """ Create new segments with the given node spacing, start location, and number of segments """
+        self.set_node_angles(start_location)
+        self.segments = self.create_segments(num_seg)
+        for seg in self.segments:
+            seg.plan_route(start_location)
 
     def earcut_triangulate(self):
         """ Triangulate the polygon using the Ear Clipping algorithm and return the triangles as a list """
-        nodes = np.array([node() for node in self.nodes]).reshape(-1, 2)
-        rings = np.array([len(nodes)])
-        result = earcut.triangulate_int32(nodes, rings)
+        nodes = np.array([node() for node in self.nodes]).reshape(-1, 2) # Convert node list to a np array
+        
+        #bbox_x = self.bounding_box["x_min"]
+        #bbox_y = self.bounding_box["y_min"]
+        #normalized_nodes = np.array(
+        #    [(node.x - bbox_x, node.y - bbox_y) for node in self.nodes],
+        #    dtype=np.float32)\
+        #.reshape(-1, 2)
+
+        rings = np.array([len(nodes)]) # Used to describe the geometry of the polygon. Can be used to define holes inside the polygon. (We don't)
+        result = earcut.triangulate_float32(nodes, rings) # List of node indices defining the triangles
         triangles = []
-        for i in range(0, len(result), 3):
+
+        for i in range(0, len(result), 3): # Iterate over all such found triangles to create the triangle objects.
+            
             triangle = Triangle([self.nodes[result[i]], self.nodes[result[i+1]], self.nodes[result[i+2]]])
             triangles.append(triangle)
 
@@ -112,14 +132,27 @@ class Polygon():
         node_grid = []
         self.bounding_box = self.create_bounding_box()
         max_diff_ang = self.max_diff_angle(start_location)
-        for x in range(self.bounding_box["x_min"], self.bounding_box["x_max"], node_spacing):
-            for y in range(self.bounding_box["y_min"], self.bounding_box["y_max"], node_spacing):
+        
+        x_min, x_max = self.bounding_box["x_min"], self.bounding_box["x_max"]
+        y_min, y_max = self.bounding_box["y_min"], self.bounding_box["y_max"]
+
+        for x in np.arange(x_min, x_max, node_spacing):
+            for y in np.arange(y_min, y_max, node_spacing):
                 for triangle in self.triangles:
-                    node = Node((x, y))
+                    node = Node((x,y))
+
+                    print("Checking...")
                     if triangle.contains(node):
+                        print("-------- All good!")
+                        # Find the zone num/letter of the closest node in the polygon node list.
+                        # This is because the nodes are created with UTM coordinates, and the lat/lon coordinates
+                        # are unknown, which means that the zone information cannot be derived.
+                        closest_poly_node = node.closest_node(self.nodes)
+                        
                         node.angle_to_start = start_location.angle_to(node)
                         node_grid.append(node)
                         break
+
         node_grid.sort(key=lambda n: (n.angle_to_start - max_diff_ang), reverse=False)
         return node_grid    
     
@@ -127,6 +160,8 @@ class Polygon():
         """ Evenly partition the amount of node grid nodes into segments 
             Node grid list is presumed to be sorted according to angle relative 'start_location' so resulting segments will be lines drawn from 'start_location'      
         """
+        assert num_seg > 0, "Number of segments must be a positive integer"
+
         total_node_count = 0
         segments = []
         num_nodes = int(np.ceil(len(self.node_grid) / num_seg))
@@ -135,8 +170,6 @@ class Polygon():
             segments.append(Segment(seg_nodes))
             total_node_count += num_nodes
         return segments
-
-
 
     def max_diff_angle(self, start_location):
         """ Calculate the difference between (start_location, a) and (start_location, b) for
@@ -152,6 +185,15 @@ class Polygon():
                     chosen_nodes = (node_a, node_b)
                     max_separation = angular_separation
         return start_location.angle_to(chosen_nodes[0])
+    
+    def set_node_angles(self, start_location):
+        def node_angle(angle_a, angle_b): # TODO: Move to util class or whatever
+            return np.mod(angle_b - angle_a + np.pi, 2*np.pi) - np.pi
+        max_diff_ang = self.max_diff_angle(start_location)
+        for node in self.node_grid:
+            node.angle_to_start = start_location.angle_to(node)
+ 
+        self.node_grid.sort(key=lambda n: node_angle(n.angle_to_start, max_diff_ang), reverse=False)
 
     @staticmethod
     def is_clockwise(nodes):
@@ -165,24 +207,77 @@ class Polygon():
             signed_area += (x2 - x1) * (y2 + y1)
 
         return signed_area <= 0
-
+        
     def __repr__(self):
         pass
 
 class Node():
-    def __init__(self, position, start_location=None):
-        self.x = position[0]
-        self.y = position[1]
-        self.angle_to_start = start_location if start_location else None
-    
+    def __init__(self, xy):
+        self.x, self.y = xy[0], xy[1]
+
+        self.angle_to_start = None
+
     def angle_to(self, other_node):
-        """ Return the angle of the node instance to the 'other_node' """
+        """ Return the angle in radians of the node instance to the 'other_node' """
         return np.arctan2(other_node.y - self.y, other_node.x - self.x)
 
     def __call__(self):
-        """ Return the coordinates when a node object is called """
+        """ Return the coordinates as a tuple when a node object is called """
         return (self.x, self.y)
+    
+    def distance_to_squared(self, other_node):
+        """ Return distance between self and another node squared """
+        return ((other_node.x-self.x)**2 + (other_node.y-self.y)**2)
+    
+    def distance_to(self, other_node):
+        """ Return distance between self and another node """
+        return np.sqrt((other_node.x-self.x)**2 + (other_node.y-self.y)**2)
+    
+    def closest_node(self, node_ls):
+        """ Returns the node closest to 'node' among 'owned_nodes' """
+        min_dist = float('inf')
+        closest_node = node_ls[0]
+        for comp_node in node_ls:
+            if comp_node is self:
+                continue
+            distance = self.distance_to(comp_node)
+            if distance < min_dist:
+                min_dist = distance
+                closest_node = comp_node
+        return closest_node
+
+    def __repr__(self):
+        """ String representation of a node object. """
+        pass
 
 class Segment():
     def __init__(self, owned_nodes):
         self.owned_nodes = owned_nodes
+        self.route = []
+
+    def plan_route(self, start_location):
+        """ Plan a route using nearest insert algorithm with a segments owned nodes """
+        start_neighbour = start_location.closest_node(self.owned_nodes)
+
+        # Insert 'start_location' as a node in the route
+        new_route = [start_location, start_neighbour]
+        unexplored_nodes = self.owned_nodes[:]
+        while (len(new_route) - 1) < len(self.owned_nodes):
+            min_insert_cost = float('inf')
+            for i in range(len(new_route) - 1):
+                node_A = new_route[i]
+                node_B = new_route[i+1]
+                for unexplored_node in unexplored_nodes:
+                    if unexplored_node in new_route:
+                        continue
+                    # Cost to insert 'unexplored_node' between 'node_A' and 'node_B'
+                    distance = node_A.distance_to(unexplored_node) + node_B.distance_to(unexplored_node)
+                    if distance < min_insert_cost:
+                        min_insert_cost = distance
+                        insert_index = i
+                        insert_node = unexplored_node
+            new_route.insert(insert_index + 1, insert_node)
+            unexplored_nodes.remove(insert_node)
+        print("Len of new route: ", len(new_route))
+        self.route = new_route              
+    
--- src/back-end/IMM/drone_allocator/ue0.py
+++ src/back-end/IMM/drone_allocator/ue0.py
@@ -3,11 +3,13 @@ import sys
 import numpy as np
 import pygame as ue
 
+import colorsys
+
 def main():
     WIDTH = 800
     HEIGHT = 600
     
-    SPACING = 5
+    SPACING = 25
     START_LOCATION = seg.Node((100, 100))
     NUM_SEG = 5
     # BYT UT SEN
@@ -19,6 +21,11 @@ def main():
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
 
+    DRAW_POLYGON = True
+    DRAW_NODE_GRID = True
+    DRAW_SEGMENTS = True
+    DRAW_TRIANGLES = False
+    DRAW_PATHS = True
 
     ue.init()
     clock = ue.time.Clock()
@@ -28,45 +35,75 @@ def main():
 
     polygon = seg.Polygon([])
 
+    colors = []
+    for i in range(NUM_SEG):
+        rgb = colorsys.hsv_to_rgb(1.0/(i+1), 1.0, 1,1)
+        colors.append((rgb[0]*255, rgb[1]*255, rgb[2]*255, 100))
+
     while True:
         clock.tick(60)
 
         surface.fill(WHITE)
         screen.blit(surface, (0,0))
 
-        for i, node in enumerate(polygon.nodes):
-            next = polygon.nodes[(i+1) % len(polygon.nodes)]
-            ue.draw.aaline(surface, BLACK, node(), next())
-            ue.draw.circle(surface, RED, node(), 5)
+        if DRAW_SEGMENTS:
+            for i, segment in enumerate(polygon.segments):
+                # -- Draw start line
+                start_angle = segment.owned_nodes[0].angle_to_start
+                start_position = seg.Node((START_LOCATION.x + line_length * np.cos(start_angle), \
+                                        START_LOCATION.y + line_length * np.sin(start_angle)))
+
+                # -- Draw end line
+                end_angle = segment.owned_nodes[-1].angle_to_start
+                end_position = seg.Node((START_LOCATION.x + line_length * np.cos(end_angle), \
+                                        START_LOCATION.y + line_length * np.sin(end_angle)))
+
+                ue.draw.polygon(surface, colors[i], (START_LOCATION(), start_position(), end_position()))
+
+        if DRAW_POLYGON:
+            for i, node in enumerate(polygon.nodes):
+                next = polygon.nodes[(i+1) % len(polygon.nodes)]
+                ue.draw.aaline(surface, BLACK, node(), next())
+                ue.draw.circle(surface, RED, node(), 5)
         
-        for triangle in polygon.triangles:
-            for i, node in enumerate(triangle.nodes()): 
-                next = triangle.nodes()[(i + 1) % 3]
-                ue.draw.aaline(surface, GREEN, node(), next())
+        if DRAW_TRIANGLES:
+            for triangle in polygon.triangles:
+                for i, node in enumerate(triangle.nodes()): 
+                    next = triangle.nodes()[(i + 1) % 3]
+                    ue.draw.aaline(surface, GREEN, node(), next())
 
-        for node in polygon.node_grid:
-            ue.draw.circle(surface, BLUE, node(), 2)
+        if DRAW_NODE_GRID:
+            for node in polygon.node_grid:
+                ue.draw.circle(surface, BLUE, node(), 2)
 
-        for segment in polygon.segments:
-            # -- Draw start line
-            start_angle = segment.owned_nodes[0].angle_to_start
-            end_position = seg.Node((START_LOCATION.x + line_length * np.cos(start_angle), \
-                                    START_LOCATION.y + line_length * np.sin(start_angle)))
-            ue.draw.aaline(surface, RED, START_LOCATION(), end_position())
+        if DRAW_SEGMENTS:
+            for segment in polygon.segments:
+                # -- Draw start line
+                start_angle = segment.owned_nodes[0].angle_to_start
+                end_position = seg.Node((START_LOCATION.x + line_length * np.cos(start_angle), \
+                                        START_LOCATION.y + line_length * np.sin(start_angle)))
+                ue.draw.aaline(surface, RED, START_LOCATION(), end_position())
 
-            # -- Draw end line
-            end_angle = segment.owned_nodes[-1].angle_to_start
-            end_position = seg.Node((START_LOCATION.x + line_length * np.cos(end_angle), \
-                                    START_LOCATION.y + line_length * np.sin(end_angle)))
-            ue.draw.aaline(surface, BLUE, START_LOCATION(), end_position())
+                # -- Draw end line
+                end_angle = segment.owned_nodes[-1].angle_to_start
+                end_position = seg.Node((START_LOCATION.x + line_length * np.cos(end_angle), \
+                                        START_LOCATION.y + line_length * np.sin(end_angle)))
+                ue.draw.aaline(surface, BLUE, START_LOCATION(), end_position())
+
+        if DRAW_PATHS:
+            for segment in polygon.segments:
+                for i in range(len(segment.route) - 1):
+                    ue.draw.aaline(surface, BLACK, segment.route[i](), segment.route[i+1]())
+                    
+                ue.draw.aaline(surface, BLACK, segment.route[0](), segment.route[-1]())
+                
 
         screen.blit(surface, (0,0))
 
         if ue.key.get_pressed()[ue.K_f] and polygon.triangles:
             mouse_pos = ue.mouse.get_pos()
             START_LOCATION = seg.Node(mouse_pos)
-            polygon.node_grid = polygon.create_node_grid(SPACING, START_LOCATION)
-            polygon.segments = polygon.create_segments(NUM_SEG)
+            polygon.update_area_segments(START_LOCATION, NUM_SEG)
 
         for event in ue.event.get():
             if event.type == ue.QUIT or ue.key.get_pressed()[ue.K_ESCAPE]:
@@ -86,16 +123,30 @@ def main():
 
             if event.type == ue.KEYDOWN:
                 if ue.key.get_pressed()[ue.K_s]:
-                    polygon.gogo_gadget(SPACING, START_LOCATION, NUM_SEG)
+                    polygon.create_area_segments(SPACING, START_LOCATION(), NUM_SEG)
                 if ue.key.get_pressed()[ue.K_t]:
                     polygon.triangles = polygon.earcut_triangulate()
                 if ue.key.get_pressed()[ue.K_g]:
-                    polygon.node_grid = polygon.create_node_grid(SPACING, START_LOCATION)
+                    polygon.node_grid = polygon.create_node_grid(SPACING, START_LOCATION())
                 if ue.key.get_pressed()[ue.K_c]:
                     polygon.nodes.clear()
                     polygon.triangles.clear()
                     polygon.node_grid.clear()
                     polygon.segments.clear()
+                if ue.key.get_pressed()[ue.K_k]:
+                    for segment in polygon.segments:
+                        segment.smooth_route()
+
+                if ue.key.get_pressed()[ue.K_1]:
+                    DRAW_POLYGON = not DRAW_POLYGON
+                if ue.key.get_pressed()[ue.K_2]:
+                    DRAW_TRIANGLES = not DRAW_TRIANGLES
+                if ue.key.get_pressed()[ue.K_3]:
+                    DRAW_NODE_GRID = not DRAW_NODE_GRID
+                if ue.key.get_pressed()[ue.K_4]:
+                    DRAW_SEGMENTS = not DRAW_SEGMENTS
+                if ue.key.get_pressed()[ue.K_5]:
+                    DRAW_PATHS = not DRAW_PATHS
 
         ue.display.update()
 
