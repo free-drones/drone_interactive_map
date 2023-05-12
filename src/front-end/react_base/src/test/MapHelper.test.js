6ee9b2968607e62eb2e8ce9bf31dbb5c062fff55
Daniel Purgal <daniel.purgal@bredband2.com>
2023-05-12 11:30:45 +0200

    add test for point in polygon

--- src/front-end/react_base/src/test/MapHelper.test.js
+++ src/front-end/react_base/src/test/MapHelper.test.js
@@ -19,14 +19,24 @@ const waypoints = [
   },
 ];
 
-const newCrossingWaypoint = {
+const point1 = {
   lat: 2,
   lng: 1,
 };
 
+const point2 = {
+  lat: 0,
+  lng: 0,
+};
+
+const point3 = {
+  lat: 1.3,
+  lng: 1.9,
+};
+
 test("Tests lines that cross", () => {
-  let testing = createRedLines(waypoints, newCrossingWaypoint, null);
-  expect(testing).toEqual([
+  let crossingLines = createRedLines(waypoints, point1, null);
+  expect(crossingLines).toEqual([
     [
       { lat: 1, lng: 2 },
       { lat: 2, lng: 1 },
@@ -36,4 +46,19 @@ test("Tests lines that cross", () => {
       { lat: 2, lng: 2 },
     ],
   ]);
+});
+
+test("Tests lines that don't cross", () => {
+  let crossingLines = createRedLines(waypoints, point2, null);
+  expect(crossingLines).toEqual([]);
+});
+
+test("Tests point that is not inside area", () => {
+  let pointInside = isPointInsidePolygon(point2, waypoints);
+  expect(pointInside).toEqual(false);
+});
+
+test("Tests point that is inside area", () => {
+  let pointInside = isPointInsidePolygon(point3, waypoints);
+  expect(pointInside).toEqual(true);
 });
