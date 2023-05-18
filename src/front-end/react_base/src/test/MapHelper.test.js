960a9c54117d36e5607ff2f851cbf0d872a953c1
Daniel Purgal <daniel.purgal@bredband2.com>
2023-05-18 12:49:04 +0200

    add new tests

--- src/front-end/react_base/src/test/MapHelper.test.js
+++ src/front-end/react_base/src/test/MapHelper.test.js
@@ -1,5 +1,4 @@
 import {
-  boundsToView,
   createRedLines,
   isPointInsidePolygon,
 } from "../JS/Helpers/maphelper";
@@ -34,7 +33,12 @@ const point3 = {
   lng: 1.9,
 };
 
-test("Tests lines that cross", () => {
+/**
+ * -------------------- CROSSING LINES TESTS --------------------
+ */
+
+
+test("Tests lines that cross when adding waypoint", () => {
   let crossingLines = createRedLines(waypoints, point1, null);
   expect(crossingLines).toEqual([
     [
@@ -48,10 +52,102 @@ test("Tests lines that cross", () => {
   ]);
 });
 
-test("Tests lines that don't cross", () => {
+test("Tests lines that don't cross when adding waypoint", () => {
   let crossingLines = createRedLines(waypoints, point2, null);
   expect(crossingLines).toEqual([]);
 });
+
+test("Tests lines that cross when adding and removing waypoint", () => {
+  let crossingLines = createRedLines(waypoints, point1, 1);
+  expect(crossingLines).toEqual([
+    [
+      { lat: 1, lng: 2 },
+      { lat: 2, lng: 1 },
+    ],
+    [
+      { lat: 1, lng: 1 },
+      { lat: 2, lng: 2 },
+    ],
+  ]);
+});
+
+test("Tests lines that don't cross when adding and removing waypoint", () => {
+  let crossingLines = createRedLines(waypoints, point2, 1);
+  expect(crossingLines).toEqual([]);
+});
+
+test("Tests lines that don't cross when removing waypoint", () => {
+  let crossingLines = createRedLines(waypoints, null, 1);
+  expect(crossingLines).toEqual([]);
+});
+
+const crossingPolygon = [
+  {
+    lat: 1,
+    lng: 1,
+  },
+  {
+    lat: 2,
+    lng: 2,
+  },
+  {
+    lat: 2.1,
+    lng: 2.1,
+  },
+  {
+    lat: 1,
+    lng: 2,
+  },
+  {
+    lat: 2,
+    lng: 1,
+  },
+  {
+    lat: 0,
+    lng: 1.5,
+  },
+];
+
+test("Tests lines that cross when removing waypoint", () => {
+  let crossingLines = createRedLines(crossingPolygon, null, 5);
+  expect(crossingLines).toEqual([
+    [
+      { lat: 1, lng: 2 },
+      { lat: 2, lng: 1 },
+    ],
+    [
+      { lat: 1, lng: 1 },
+      { lat: 2, lng: 2 },
+    ],
+  ]);
+});
+
+test("Tests lines that cross when removing waypoint", () => {
+  let crossingLines = createRedLines(crossingPolygon, null, 2);
+  expect(crossingLines).toEqual([
+    [
+      { lat: 1, lng: 2 },
+      { lat: 2, lng: 1 },
+    ],
+    [
+      { lat: 2, lng: 1 },
+      { lat: 0, lng: 1.5 },
+    ],
+    [
+      { lat: 1, lng: 1 },
+      { lat: 2, lng: 2 },
+    ],
+    [
+      { lat: 1, lng: 1 },
+      { lat: 2, lng: 2 },
+    ],
+  ]);
+});
+
+
+/**
+ * -------------------- POINT IN POLYGON TESTS --------------------
+ */
 
 test("Tests point that is not inside area", () => {
   let pointInside = isPointInsidePolygon(point2, waypoints);
